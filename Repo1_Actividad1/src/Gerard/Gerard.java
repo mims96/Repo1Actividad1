b667fc53bfb9630859ae9f22c6209b92c9fc17ab
Gerard.Perujo <ger.pe85@gmail.com>
2023-01-28 18:11:05 +0100

    creado soy Gerard

---
+++ Repo1_Actividad1/src/Gerard/Gerard.java
@@ -0,0 +1,32 @@
+package Gerard;
+
+public class Gerard {
+	
+	 
+	 
+	 public static void main (String[]args) {
+		String nombre = "Gerard"; 
+		
+		llamarNombre(nombre);
+		
+		System.out.println("Hola, soy " + nombre);
+		
+		
+	 }
+
+	
+
+
+public static String llamarNombre(String nombre) {
+	return nombre;
+}
+
+}	
+
+	
+	
+	
+
+
+
+
