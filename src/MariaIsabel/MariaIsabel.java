ddf51bb46d29c3187583fee905d2baf77e4081a8
Maria Isabel <contacto.mims@gmail.com>
2023-01-28 18:19:35 +0100

    Desarrollo punto 6 requerimiento 1

---
+++ src/MariaIsabel/MariaIsabel.java
@@ -0,0 +1,16 @@
+package MariaIsabel;
+
+public class MariaIsabel {
+	public static void main(String[] args) {
+		String nombre = "Maria Isabel";
+		llamarNombre (nombre);
+		System.out.println("Hola, soy " + nombre);
+		
+	}
+	public static String llamarNombre (String nombre) {
+		return nombre;
+	}
+	
+}
+	
+
